f154762fe089621715a23d76e59f1cdeef30a66b
kissrekax <144942169+kissrekax@users.noreply.github.com>
2023-09-20 14:55:43 +0200

    Update builtin_wrappers.py

---
+++ src/utils/builtin_wrappers.py
@@ -0,0 +1,12 @@
+def odds_from_list(input_list):
+    odd_elements = [x for x in input_list if x % 2 != 0]
+    return odd_elements
+
+def reverse_list(input_list):
+    reversed_list = input_list[::-1]
+    return reversed_list
+
+def sum_in_dict(input_dict):
+    total = sum(input_dict.values())
+    return total
+
