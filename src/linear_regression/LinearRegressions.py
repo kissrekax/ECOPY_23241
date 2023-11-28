cfcbb85a7c33f0a9206692d6fd60eda42f2df40a
kissrekax <144942169+kissrekax@users.noreply.github.com>
2023-11-28 17:01:27 +0100

    Update LinearRegressions.py

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -167,54 +167,45 @@ class LinearRegressionML:
         self.right_hand_side.insert(0, 'alfa', 1)
         self.left_hand_side = self.left_hand_side.values
         self.right_hand_side = self.right_hand_side.values
-        self.n = len(self.left_hand_side)
+        self.params = None
 
-    def loglikelihood(self, params):
-        X = self.right_hand_side
-        Y = self.left_hand_side
-        beta = params[:-1]
-        sigma_squared = params[-1]
-        n = len(self.left_hand_side)
-        residuals = Y - X@beta
-        log_likelihood = (-n/2*np.log(2*np.pi))-(n/2*np.log(sigma_squared)) - (residuals.T@residuals)/2*sigma_squared
+    def log_likelihood(params, X, y):
+        predicted = X @ params
+        log_likelihood = -0.5 * (np.log(2 * np.pi * np.var(y)) + np.sum((y - predicted) ** 2) / np.var(y))
         return -log_likelihood
 
     def fit(self):
-        initial = [0.1, 0.1, 0.1, 0.1, 0.1]
-        res = scipy.optimize.minimize(self.loglikelihood, initial, method='L-BFGS-B')
-        estimated_params = res.x
-        self.beta_hat = estimated_params[:-1]
-        self.sigma_squared_hat = estimated_params[-1]
+        def log_likelihood(params, X, y):
+            predicted = X @ params
+            log_likelihood = -0.5 * (np.log(2 * np.pi * np.var(y)) + np.sum((y - predicted) ** 2) / np.var(y))
+            return -log_likelihood
 
-        return
+        initial_params = np.zeros(self.right_hand_side.shape[1]) + 0.1
+
+        self.result = scipy.optimize.minimize(log_likelihood, initial_params,args=(self.right_hand_side, self.left_hand_side), method='L-BFGS-B')
+        self.params = self.result.x
+        self.hess_inv = self.result.hess_inv
 
     def get_params(self):
-        return pd.Series(self.beta_hat, name='Beta coefficients')
+        return pd.Series(self.params, name='Beta coefficients')
 
     def get_pvalues(self):
-        self.residuals = self.left_hand_side - np.dot(self.right_hand_side, self.betas)
-        self.n = len(self.left_hand_side)
-        self.K = len(self.right_hand_side.columns)
+        self.residuals_ml = self.left_hand_side - self.right_hand_side @ self.params
+        self.n = self.right_hand_side.shape[0]
+        self.K = self.right_hand_side.shape[1]
         self.df = self.n - self.K
-        self.variance = self.residuals.T@self.residuals / self.df
-        self.stderror = np.sqrt(self.variance * np.diag(self.XtX_inv))
-        self.t_stat = np.divide(self.betas, self.stderror)
-        term = np.minimum(scipy.stats.t.cdf(self.t_stat, self.df), 1 - scipy.stats.t.cdf(self.t_stat, self.df))
+        self.variance_ml = (self.residuals_ml.T @ self.residuals_ml) / self.n
+        self.std_errors_ml = np.sqrt(np.abs(np.diag(self.hess_inv)))
+        self.std_error_ml_2 = np.sqrt(self.variance_ml * np.diag(np.linalg.inv(self.right_hand_side.T @ self.right_hand_side)))
+        self.t_stat_ml = np.divide(self.params, self.std_errors_ml)
+        term = np.minimum(scipy.stats.t.cdf(self.t_stat_ml, self.df), 1 - scipy.stats.t.cdf(self.t_stat_ml, self.df))
         p_values = term * 2
         return pd.Series(p_values, name='P-values for the corresponding coefficients')
 
-    def get_wald_test_result(self, restr_matrix):
-        term_1 = restr_matrix@self.betas
-        term_2 = np.linalg.inv(restr_matrix@self.XtX_inv@np.array(restr_matrix).T)
-        m = len(restr_matrix)
-        f_stat = (term_1.T@term_2@term_1/m)/self.variance
-        p_value = 1 - scipy.stats.f.cdf(f_stat, m, self.df)
-        return f'Wald: {f_stat:.3f}, p-value: {p_value:.3f}'
-
     def get_model_goodness_values(self):
         y_demean = self.left_hand_side - self.left_hand_side.mean()
-        SST = y_demean.T@y_demean
-        SSE = self.residuals.T@self.residuals
+        SST = y_demean.T @ y_demean
+        SSE = self.residuals_ml.T @ self.residuals_ml
         r2 = 1 - SSE / SST
         adj_r2 = 1 - (self.n - 1) / self.df * (1 - r2)
         return f'Centered R-squared: {r2:.3f}, Adjusted R-squared: {adj_r2:.3f}'
